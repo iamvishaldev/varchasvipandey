bb82c9643fa2250c93cab235b8db3168a19480a2
varchasvipandey <varchasvip@gmail.com>
2020-06-08 19:17:31 +0530

    fixed blog2 typo

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -10,7 +10,7 @@ class Blogs extends Component {
         title: "Quick MERN application & a way to Cross the CORS",
         sub: "Quick MERN stack guide",
         content:
-          "You might be familiar with a tern CORS. If not then I guess you haven’t worked much with APIs and requests. Let’s start with a formal introduction of CORS (from Wikipedia): Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served.",
+          "You might be familiar with a term CORS. If not then I guess you haven’t worked much with APIs and requests. Let’s start with a formal introduction of CORS (from Wikipedia): Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served.",
         link: "https://bit.ly/mern-cors"
       },
       {
