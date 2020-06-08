70083ba01a8750abb1b3e3e23077db0d8b3b1687
varchasvipandey <varchasvip@gmail.com>
2020-06-08 16:26:07 +0530

    second blog

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -7,6 +7,13 @@ class Blogs extends Component {
   state = {
     blogs: [
       {
+        title: "Quick MERN application & a way to Cross the CORS",
+        sub: "Quick MERN stack guide",
+        content:
+          "You might be familiar with a tern CORS. If not then I guess you haven’t worked much with APIs and requests. Let’s start with a formal introduction of CORS (from Wikipedia): Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served.",
+        link: "https://bit.ly/mern-cors"
+      },
+      {
         title: "Asynchronous array behaviour in JavaScript",
         sub: "Truth behind empty but full array",
         content:
